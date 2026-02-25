95541c751fc4999f6f58ffb0a72c7ea539df625d
3rrat3 <mohammedhazim2301@gmail.com>
2026-02-25 04:07:35 +0300

    Implement slugify

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,32 @@
 export function slugify(input: string): string {
-  return "placeholder-slug";
+  // Trim + LowerCase
+  input = input.toLocaleLowerCase().trim();
+
+  // Check character at index i
+  function isCharOrNumber(character: string): boolean {
+    // convert to ascii value
+    let char: number = character.charCodeAt(0);
+    // check if it's a number
+    let answer: boolean = char >= 48 && char <= 57;
+    // check if it isn't english letter
+    answer ||= /^\p{L}$/u.test(character);
+
+    return answer;
+  }
+
+  for (let i = 0; i < input.length; i++) {
+    if (input[i] === " ") continue;
+    // Ignore special characters
+    if (!isCharOrNumber(input[i])) {
+      input = input.slice(0, i) + "" + input.slice(i + 1);
+      i--;
+    }
+  }
+  // console.log(s);
+
+  input = input.replace(/ /g, "-"); // replace all spaces with -
+  input = input.replace(/-+/g, "-"); // remove - and keep one
+  input = input.replace(/^-+|-+$/g, ""); // remove - at start/end
+  // console.log(s);
+  return input;
 }
